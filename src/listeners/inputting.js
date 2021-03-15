cb3d7627493fb65d97644b8f7fe9d5a703740802
Mahmood <pejvakco@gmail.com>
2021-03-15 21:31:21 +0100

    delete antify

--- src/listeners/inputting.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/**
- * @name user input
- * calls the reverseHandler when a user types in the input field
- */
- import { artifyHandler } from '../handlers/artify.js';
-document.getElementById('input').addEventListener('keyup', artifyHandler);
